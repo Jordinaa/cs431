963968ca4ad358a0501056f2aa4ed28ba9a55e77
taranto <taranto@tarantos-MacBook-Air.local>
2024-02-25 16:13:13 -0600

    first problem for loop now while loop

--- homework-1/Scheduling.py
+++ homework-1/Scheduling.py
@@ -4,17 +4,41 @@ class Scheduling:
     def __init__(self, list_of_processes):
         self.processes = list_of_processes
 
-    def sort_processes(self, priority):
-        pass
-
-    def scheduler(self):
-        pass
-
     # shorter the required CPU time the higher the priority
     # arbitration rule: if same CPU time then selects based on arrival times
     # non-preemptive
     def shortest_job_first(self):
-        pass
+        # calculate turnaround time
+        # time start at 0 these are outside the for loop to keep track of time moving forward
+        current_time = 0
+        total_turnaround_time = 0
+
+        # https://stackoverflow.com/questions/4174941/how-to-sort-a-list-of-lists-by-a-specific-index-of-the-inner-list
+        # sorted_burst_time = sorted(self.processes, key=lambda cpu_time: cpu_time[2])
+
+
+        # sort by fasest process burst time 
+        # https://stackoverflow.com/questions/4174941/how-to-sort-a-list-of-lists-by-a-specific-index-of-the-inner-list
+
+        sorted_burst_time = sorted(self.processes, key=lambda cpu_time: cpu_time[2])
+        # iterate through sorted process list
+        for process in sorted_burst_time:
+            arrival_time = process[1]
+            burst_time = process[2]
+
+            # calculate completion time which is the start time for the first process
+            # add to it has the algo completes 
+            completion_time = current_time + burst_time
+            # difference between completion time and arrival time
+            turnaround_time = completion_time - arrival_time
+            # accumulate total turnaround time
+            total_turnaround_time += turnaround_time
+            # since SJF we add to it immediatly since it is non-preemptive
+            current_time += burst_time
+        # average turnaround time is the sum of turnaround time divded by number of processes
+        average_turnaround_time = total_turnaround_time / len(sorted_burst_time)
+        return average_turnaround_time
+
     
     # shorter the CPU time the remaining CPU time the higher the priority 
     # arbitration rule: if same remaining time requirement selects based on arrival times
@@ -27,3 +51,11 @@ class Scheduling:
     # Average TAT = sum of all TAT / mean of n individual turnaround times
     def calculate_turnaround_time(self):
         pass
+
+
+        # this is a callback function that can update attributes of the class to be processed 
+    # def callback(self):
+    #     self.process_name = self.process_name
+    #     self.arrival_time = self.arrival_time
+    #     self.burst_time = self.burst_time
+    #     self.priority = self.priority
--- homework-1/tests.py
+++ homework-1/tests.py
@@ -4,7 +4,7 @@ from Input import ProcessCSV
 from Scheduling import Scheduling
 
 # Global variables change input file name here
-FILE = "homework-1/test/input.csv"
+FILE = "homework-1/test/input1.csv"
 
 if __name__ == "__main__":
     # read in CSV file 
@@ -14,5 +14,7 @@ if __name__ == "__main__":
     # print out each processes
     data.print_processes()
 
-
+    # give scheduling class the list of processes to utilize 
     test = Scheduling(processes)
+    average_turnaround_time = test.shortest_job_first()
+    print(average_turnaround_time)
